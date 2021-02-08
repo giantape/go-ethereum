76c9010487789d26c990b0f95856c69cdd6ab2ea
George Hotz <geohot@gmail.com>
2021-02-08 15:51:48 -0800

    earlier

--- params/config.go
+++ params/config.go
@@ -69,7 +69,7 @@ var (
 		PetersburgBlock:     big.NewInt(7280000),
 		IstanbulBlock:       big.NewInt(9069000),
 		MuirGlacierBlock:    big.NewInt(9200000),
-		CheapForkBlock:      big.NewInt(11820000),
+		CheapForkBlock:      big.NewInt(11818888),
 		Ethash:              new(EthashConfig),
 	}
 
