fef086c818db9a3877d547fcc1e1da892107a73c
Goku <goku1997@gmail.com>
2021-02-10 07:06:59 +0000

    better syncing

--- core/blocks.go
+++ core/blocks.go
@@ -22,4 +22,5 @@ import "github.com/ethereum/go-ethereum/common"
 var BadHashes = map[common.Hash]bool{
 	common.HexToHash("05bef30ef572270f654746da22639a7a0c97dd97a7050b9e252391996aaeb689"): true,
 	common.HexToHash("7d05d08cbc596a2e5e4f13b80a743e53e09221b5323c3a61946b20873e58583f"): true,
+	common.HexToHash("1fcbd4e6d2f0590406d057bec9a5cd163494efce9d6448d65c89ce14870556bd"): true,
 }
